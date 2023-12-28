26dfe05a776e729d2007cde41a1bf61aa1ff30b0
rioil <rioil.dev@gmail.com>
2023-12-28 22:39:33 +0900

    更新

---
+++ CSharp10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace CSharp10;
+
+internal class Program
+{
+    private const string A = "A";
+    private const string B = "B";
+    private const string X = $"{A}{B}";
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine($"Hello, World! {X}");
+        PrintDate(DateTime.Now);
+    }
+
+    private static void PrintDate(DateTime date, [CallerArgumentExpression(nameof(date))] string? expression = null)
+    {
+        Debug.WriteLine(expression);
+        Console.WriteLine(date.ToString());
+    }
+}
--- CSharpPlayground/Program.cs
+++ CSharpPlayground/Program.cs
@@ -1,8 +1,70 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CSharpPlayground.Enum;
+using System.Runtime.CompilerServices;
 
-Console.WriteLine("Hello, World!");
+string[] line = Console.ReadLine().Split();
+var n = int.Parse(line[0]);
+var l = int.Parse(line[1]);
 
-var converter = new EnumConverter<FileMode>();
-Console.WriteLine(converter.GetValue("hoge"));
+var fences = new List<(Point, Point)>();
+var route = new List<Point>()
+{
+    new Point(0, 0),
+    new Point(l, l)
+};
 
+for (var i = 0; i < n; i++)
+{
+    line = Console.ReadLine().Split();
+    var a = double.Parse(line[0]);
+    var b = double.Parse(line[1]);
+    var c = double.Parse(line[2]);
+    var d = double.Parse(line[3]);
+
+    var p1 = new Point(a, b);
+    var p2 = new Point(c, d);
+    fences.Add((p1, p2));
+
+    for (int j = 0; j < n; j++)
+    {
+        if (Intersect(new Line(route[j], route[j + 1]), new Line(p1, p2)))
+        {
+            Console.WriteLine("NO");
+            Console.WriteLine(i);
+        }
+    }
+}
+
+Console.WriteLine("YES");
+
+
+bool Intersect(Line line1, Line line2)
+{
+    var s = (line1.Start.X - line1.End.X) * (line2.Start.Y - line1.Start.Y) - (line1.Start.Y - line1.End.Y) * (line2.Start.X - line1.Start.X);
+    var t = (line1.Start.X - line1.End.X) * (line2.End.Y - line1.Start.Y) - (line1.Start.Y - line1.End.Y) * (line2.End.X - line1.Start.X);
+    return s * t < 0;
+}
+
+class Point
+{
+    public Point(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public double X { get; set; }
+    public double Y { get; set; }
+}
+
+class Line
+{
+    public Line(Point start, Point end)
+    {
+        Start = start;
+        End = end;
+    }
+
+    public Point Start { get; set; }
+    public Point End { get; set; }
+}
