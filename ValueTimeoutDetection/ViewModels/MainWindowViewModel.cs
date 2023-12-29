5311eb56925fc1c174f65db1e97cd5992ca2b4be
rioil <rioil.dev@gmail.com>
2023-12-29 12:41:58 +0900

    ValueTimeoutDetectionを更新

--- ValueTimeoutDetection/ViewModels/MainWindowViewModel.cs
+++ ValueTimeoutDetection/ViewModels/MainWindowViewModel.cs
@@ -12,13 +12,17 @@ namespace ValueTimeoutDetection.ViewModels
         {
             var timeout = TimeSpan.FromSeconds(3);
             CompositeDisposable.Add(MyProperty.Where(x => x).Subscribe(x => MyProperty.Value = false));
-            MyPropertyTriggeredTime = MyProperty.Select(x => DateTime.Now).ToReadOnlyReactivePropertySlim();
-            
+            MyPropertyTriggeredTime = MyProperty.Timestamp()
+                                                .Select(x => x.Timestamp.LocalDateTime)
+                                                .ToReadOnlyReactivePropertySlim();
+
             IsMyPropertyExpired = Observable.Merge(
                 MyProperty.Where(x => x).Select(x => false),
                 MyProperty.Where(x => x).Throttle(timeout).Select(x => true))
                 .ToReadOnlyReactivePropertySlim(true);
-            MyPropertyExpiredTime = IsMyPropertyExpired.Select(x => DateTime.Now).ToReadOnlyReactivePropertySlim();
+            MyPropertyExpiredTime = IsMyPropertyExpired.Timestamp()
+                                                       .Select(x => x.Timestamp.LocalDateTime)
+                                                       .ToReadOnlyReactivePropertySlim();
         }
 
         // Some useful code snippets for ViewModel are defined as l*(llcom, llcomn, lvcomm, lsprop, etc...).
