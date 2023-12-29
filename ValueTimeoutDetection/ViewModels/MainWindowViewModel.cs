14c222b9dad00005ae2d8c3e8910b599cddc38fc
rioil <rioil.dev@gmail.com>
2023-12-29 12:05:40 +0900

    ValueTimeoutDetectionを追加

---
+++ ValueTimeoutDetection/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Livet;
+using Reactive.Bindings;
+using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace ValueTimeoutDetection.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        public MainWindowViewModel()
+        {
+            var timeout = TimeSpan.FromSeconds(3);
+            CompositeDisposable.Add(MyProperty.Where(x => x).Subscribe(x => MyProperty.Value = false));
+            MyPropertyTriggeredTime = MyProperty.Select(x => DateTime.Now).ToReadOnlyReactivePropertySlim();
+            
+            IsMyPropertyExpired = Observable.Merge(
+                MyProperty.Where(x => x).Select(x => false),
+                MyProperty.Where(x => x).Throttle(timeout).Select(x => true))
+                .ToReadOnlyReactivePropertySlim(true);
+            MyPropertyExpiredTime = IsMyPropertyExpired.Select(x => DateTime.Now).ToReadOnlyReactivePropertySlim();
+        }
+
+        // Some useful code snippets for ViewModel are defined as l*(llcom, llcomn, lvcomm, lsprop, etc...).
+        public void Initialize()
+        {
+        }
+
+        public ReactiveProperty<bool> MyProperty { get; } = new();
+        public ReadOnlyReactivePropertySlim<DateTime> MyPropertyTriggeredTime { get; }
+
+        public ReadOnlyReactivePropertySlim<bool> IsMyPropertyExpired { get; }
+        public ReadOnlyReactivePropertySlim<DateTime> MyPropertyExpiredTime { get; }
+    }
+}
