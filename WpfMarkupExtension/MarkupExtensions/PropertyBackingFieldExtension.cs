a2513ffad269ebb8afb41146ccb04bc159e08fe8
rioil <rioil.dev@gmail.com>
2023-12-29 10:16:42 +0900

    MarkupExtensionを追加

---
+++ WpfMarkupExtension/MarkupExtensions/PropertyBackingFieldExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace WpfMarkupExtension.MarkupExtensions
+{
+    // ref. https://learn.microsoft.com/en-us/dotnet/api/system.windows.markup.markupextension.providevalue?view=windowsdesktop-8.0
+    internal class PropertyBackingFieldExtension : MarkupExtension
+    {
+        private readonly string _propertyPath;
+
+        public PropertyBackingFieldExtension(string path)
+        {
+            _propertyPath = path;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var valueTarget = serviceProvider.GetService<IProvideValueTarget>();
+            if (valueTarget is null) { return string.Empty; }
+
+            if (valueTarget.TargetObject is not FrameworkElement frameworkElement || frameworkElement.DataContext is null)
+            {
+                return string.Empty;
+            }
+
+            return string.Empty;
+        }
+    }
+
+    internal static class IServiceProviderExtensions
+    {
+        public static T? GetService<T>(this IServiceProvider serviceProvider) where T : class
+        {
+            return serviceProvider.GetService(typeof(T)) as T;
+        }
+    }
+}
---
+++ WpfMarkupExtension/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Livet;
+using Livet.Commands;
+using Livet.EventListeners;
+using Livet.Messaging;
+using Livet.Messaging.IO;
+using Livet.Messaging.Windows;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using WpfMarkupExtension.Models;
+
+namespace WpfMarkupExtension.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        // Some useful code snippets for ViewModel are defined as l*(llcom, llcomn, lvcomm, lsprop, etc...).
+        public void Initialize()
+        {
+        }
+
+        public string MyProperty
+        {
+            get => _myProperty;
+            set => RaisePropertyChangedIfSet(ref _myProperty, value);
+        }
+        private string _myProperty = string.Empty;
+    }
+}
