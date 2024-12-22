29e10e37f13319d3860da72fcae7e046783de4f8
rioil <rioil.dev@gmail.com>
2024-12-22 18:34:26 +0900

    3D図形の描画サンプルをユーザーコントロール化

---
+++ Wpf3D/Controls/Sample3DGeometry.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf3D.Controls
+{
+    /// <summary>
+    /// Sample3DGeometry.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Sample3DGeometry : UserControl
+    {
+        public Sample3DGeometry()
+        {
+            InitializeComponent();
+        }
+    }
+}
